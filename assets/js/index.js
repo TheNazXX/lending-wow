7201ff60ae7a4908dcd464d164c1f750f93e647a
Nazar <nz.shevchuk.2002@gmail.com>
2023-08-16 17:41:24 +0300

    main-section + resp

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,27 @@
+(function(){
+  const burgerMenuBtn = document.querySelector(".js-burger");
+
+  if(burgerMenuBtn){
+    burgerMenuBtn.addEventListener("click", (e) => {
+      burgerMenuBtn.classList.toggle("burger--active")
+    })
+  }
+})();
+
+
 const swiperContainer = new Swiper('.swiper', {
+  pagination: "true",
   slidesPerView: "auto",
-  spaceBetween: "30",
-  pagination: "true"
+  navigation: {
+    nextEl: '.js-swiper-button-next',
+    prevEl: '.js-swiper-button-prev'
+  },
+  pagination: {
+    el: '.js-swiper-pagination', // Элемент для пагинации
+    clickable: true, // Делает пагинацию кликабельной
+  }
 });
+
 
 
 
